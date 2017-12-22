95a8e70fce8436f8a0264db7647d34e489297034
disha <dishaagrawal1358@gmail.com>
2017-12-22 22:52:31 +0530

    Second commit

---
+++ src/main/java/personalChat/Configuration/MyMessage.java
@@ -0,0 +1,31 @@
+package personalChat.Configuration;
+
+
+//Model of message type send by user.
+public class MyMessage 
+{
+	String from;
+	String content;
+	String to;
+	
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
